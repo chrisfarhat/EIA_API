365d5bea46257d972f3eeba5d349d9ab3ae42835
jhamski <hamski.2@gmail.com>
2012-12-30 12:28:58 -0500

    R script accesses EIA API
    Using an unique API key, this script downloads JSON datafile for yearly
    records of Texas average retial electricity prices and plots output.

---
+++ EIA_API_query.R
@@ -0,0 +1,36 @@
+# script to access the EIA's beta API, convert data, then plot
+# requires a unique API key which can be obtained at: http://www.eia.gov/beta/api/
+
+
+#series
+series = "ELEC.PRICE."
+#Two letter state abbreviation
+state = "TX"
+#Type All = All, Residential = 
+type = "ALL"
+#Time A = Annual, M = Monthly, Q = Quarterly
+time = "A"
+#API Key
+key <- "*******************"
+
+require(RCurl)
+require(RJSONIO)
+
+#Create API url 
+id<-paste(series,state,"-",type,".",time, sep="")
+api<-paste("http://api.eia.gov/series/data?api_key=",key,"&series_id=",id, sep="")
+
+#Get data via RCurl
+data_json<-getURL(api, followlocation = TRUE)
+
+#Convert JSON format to R format, then flatten structure
+data_nested<-fromJSON(data_json, nullValue="NA")[[2]]
+
+data<- data.frame(
+	year = as.numeric(rapply(data_nested, function(x) x[1], how= "unlist")),
+	price = as.numeric(rapply(data_nested, function(x) x[2], how= "unlist")))
+data<-data[-1,]
+
+plot(data, main = id, type = "b")
+
+}
