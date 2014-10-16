a6bdc321cb9139900b93e22567dc44ea83a753b2
jhamski <hamski.2@gmail.com>
2014-10-15 22:08:27 -0400

    category query started

---
+++ EIA_API_cat query.R
@@ -0,0 +1,22 @@
+
+
+#API Key
+key <- scan("/Users/jim/Documents/R/private_EIA_API_key.txt", what = "character")
+
+#Create API url 
+api<-paste("http://api.eia.gov/category/?api_key=",key,"&category_id=371", sep="")
+
+#Get data via RCurl
+data_json<-getURL(api, followlocation = TRUE)
+
+data_nested <- fromJSON(data_json)
+data_flat <- flatten(as.data.frame(data_nested))
+
+#Create API url 
+api<-paste("http://api.eia.gov/category/?api_key=",key,"&category_id=293779", sep="")
+
+#Get data via RCurl
+data_json<-getURL(api, followlocation = TRUE)
+
+data_nested <- fromJSON(data_json)
+data_flat <- flatten(as.data.frame(data_nested))
